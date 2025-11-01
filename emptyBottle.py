f3d678d906d19b993974b95e5da432d85feff421
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2025-11-01 20:26:07 +0000

    recursion added

--- emptyBottle.py
+++ emptyBottle.py
@@ -1,9 +1,11 @@
+drank=0
 def drink(numBottles, exchangeBottles):
+    global drank
     if numBottles<exchangeBottles:
-        return(0)
+        return(1)
     else:
-    return(drink(numBottles/exchangeBottles,exchangeBottles))
-
+        return(drink(numBottles/exchangeBottles,exchangeBottles)+drank)
 numBottles=9
 exchangeBottles=3
-drink(numBottles, exchangeBottles)
+print(drink(numBottles, exchangeBottles))
+
